fb028d68415e9eb76d16a26cd634eedcad80d924
Sambour <543304915@qq.com>
2020-02-11 15:18:41 -0800

    deleting catkin_ws

--- catkin_ws/build/turtle_house/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anubhav/catkin_ws/src/turtle_house
-# Build directory: /home/anubhav/catkin_ws/build/turtle_house
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
